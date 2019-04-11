23e385a5dca5c6fdee29dcdec1750c76fbbbe284
Ruslan Yuldashev <ruslan@dcode.team>
2019-04-11 22:27:57 +0500

    First TG bot + token

---
+++ tg.py
@@ -0,0 +1,9 @@
+'''
+Here is the module for testing functions and telegram integration
+'''
+
+import telegram
+bot = telegram.Bot(token='656974486:AAFhijLPV10bnge-n8buRhRxKsfpaDYP43I')  # it's a private! Don't steal it!
+
+
+print(bot.get_me())
